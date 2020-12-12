bd933a5fec7df9e430b9af1155f1703092bf32aa
Chiho99 <chihomikami.81@gmail.com>
2020-12-12 22:22:20 +0900

    add http request

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def signupfunc(request):
+    if request.method == "POST":
+        print('This is post function')
+    else:
+        print('This is not post method')
     return render(request, 'signup.html', {'some':100})
