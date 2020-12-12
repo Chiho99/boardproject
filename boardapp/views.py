7bfb7f2706b5504d2fc638ab9b193e63c4d73935
Chiho99 <chihomikami.81@gmail.com>
2020-12-12 22:36:18 +0900

    add user model

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 def signupfunc(request):
+
+    # Userモデルから取得したデータをobject_listに格納している
+    object_list = User.objects.all()
+    print(object_list)
+    if request.method == "POST":
+        print('This is post function')
+    else:
+        print('This is not post method')
     return render(request, 'signup.html', {'some':100})
+
