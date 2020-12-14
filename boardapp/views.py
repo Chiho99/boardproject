6dc1dad9e069c6b07d43c0ee9be70a3ffbd1f169
Chiho99 <chihomikami.81@gmail.com>
2020-12-14 17:59:39 +0900

    set error handle

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 # Create your views here.
 def signupfunc(request):
-    print(request.POST)
-    # Userモデルから取得したデータをobjectに格納している
-    object = User.objects.get(username="chiho")
-    print(object.email)
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
+        # error handling
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some':100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーは既に登録されています'}) 
     return render(request, 'signup.html', {'some':100})
 
