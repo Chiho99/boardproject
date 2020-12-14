4b2cb9a84fef6a2408121d4150294f0b03575b66
Chiho99 <chihomikami.81@gmail.com>
2020-12-14 21:18:53 +0900

    make model

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_user = models.CharField(max_length=50)
+    imgs = models.ImageField(upload_to='') #settings.pyで設定したところに保存される
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.TextField()
