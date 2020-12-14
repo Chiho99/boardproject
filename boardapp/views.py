5d7e88b7e084a71bae3e30a1ea0c3137afb43955
Chiho99 <chihomikami.81@gmail.com>
2020-12-14 16:50:36 +0900

    add create user function

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 # Create your views here.
 def signupfunc(request):
-
+    print(request.POST)
     # Userモデルから取得したデータをobjectに格納している
     object = User.objects.get(username="chiho")
     print(object.email)
     if request.method == "POST":
-        print('This is post function')
-    else:
-        print('This is not post method')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
     return render(request, 'signup.html', {'some':100})
 
