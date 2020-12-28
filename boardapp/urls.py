5cd38fae9140342f8e838a6bf71615c207d408be
Chiho99 <chihomikami.81@gmail.com>
2020-12-29 00:38:13 +0900

    modify front-end

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc, Boardcreate
 
 urlpatterns = [
+    path('', signupfunc),
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('good/<int:pk>', goodfunc, name='good'),
     path('read/<int:pk>', readfunc, name='read'),
     path('create/', Boardcreate.as_view(), name='create')
+    
 ]
